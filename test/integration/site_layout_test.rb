afad264a54b2ca150cbd4965b2f42fce0fc3cfb9
abla <abla00@gmail.com>
2016-05-20 07:44:48 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:abla)
+  end
+  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -11,5 +15,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+  
+  test "layout links after logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select 'a[href=?]', user_path(@user), text: "Profile"
+    assert_select 'a[href=?]', edit_user_path(@user), text: "Settings"
+    assert_select "a[href=?]", logout_path
   end
 end
