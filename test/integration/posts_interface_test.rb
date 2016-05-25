791cddd1c645444acb213d8210631a588231cb07
abla <abla00@gmail.com>
2016-05-25 08:59:16 +0000

    creating and deleting posts

---
+++ test/integration/posts_interface_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class PostsInterfaceTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:abla)
+  end
+
+  test "post interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    # Invalid submission
+    assert_no_difference 'Post.count' do
+      post posts_path, post: { content: "" }
+    end
+    assert_select 'div#error_explanation'
+    # Valid submission
+    title   = "Valid title"
+    content = "This post really ties the room together"
+    price   = 10
+    assert_difference 'Post.count', 1 do
+      post posts_path, post: { title: title, content: content, price: price }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+    # Delete a post.
+    assert_select 'a', text: 'delete'
+    first_post = @user.posts.paginate(page: 1).first
+    assert_difference 'Post.count', -1 do
+      delete post_path(first_post)
+    end
+    # Visit a different user.
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+  end
+end
