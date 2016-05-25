5495a27aa34f1443433858262348c8c06f78c55b
abla <abla00@gmail.com>
2016-05-25 10:31:18 +0000

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'bcrypt', '3.1.7'
 
 # make sample users with semi-realistic names and email addresses
 gem 'faker'
+gem 'carrierwave',             '0.10.0'
+gem 'mini_magick',             '3.8.0'
+gem 'fog',                     '1.36.0'
 gem 'will_paginate',           '3.0.7'
 gem 'bootstrap-will_paginate', '0.0.10'
 
--- test/integration/posts_interface_test.rb
+++ test/integration/posts_interface_test.rb
@@ -10,18 +10,25 @@ class PostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Post.count' do
-      post posts_path, post: { content: "" }
+      post posts_path, post: { title: "", content: "", price: nil }
     end
     assert_select 'div#error_explanation'
     # Valid submission
     title   = "Valid title"
     content = "This post really ties the room together"
     price   = 10
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Post.count', 1 do
-      post posts_path, post: { title: title, content: content, price: price }
+      post posts_path, post: { title:   title,
+                               content: content,
+                               price:   price,
+                               picture: picture }
     end
+    foo = assigns(:post)
+    assert foo.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -34,5 +41,19 @@ class PostsInterfaceTest < ActionDispatch::IntegrationTest
     # Visit a different user.
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
+  end
+  
+  test "post sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.posts.count} posts", response.body
+    # User with zero posts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 posts", response.body
+    other_user.posts.create!(title: "test", content: "A micropost", price:5)
+    get root_path
+    assert_match "1 post", response.body
   end
 end
